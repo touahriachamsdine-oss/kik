ba7fb79f4c2c32e151c462a74b91656a1a1a3e49
Touahria Chamse <ironchamse@gmail.com>
2025-12-06 18:48:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1997920510-d7dcb",
+  "appId": "1:197326756222:web:2fc755f38f243159c2fb2f",
+  "apiKey": "AIzaSyAuQjERkI_BtGljU8y0kf2-bKstByeZUms",
+  "authDomain": "studio-1997920510-d7dcb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "197326756222"
+};
