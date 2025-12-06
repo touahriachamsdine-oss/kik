1239d271f88610fec98cd2f73c7b3a83bdb38be5
Touahria Chamse <ironchamse@gmail.com>
2025-12-06 18:44:00 +0000

    # دفتر الشروط التقني - تطبيق رحلاتي + ## منصة حجز وإدارة الرحلات السياحي

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Tajawal', 'sans-serif'],
+        headline: ['Cairo', 'serif'],
         code: ['monospace'],
       },
       colors: {
